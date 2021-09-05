03198b064eb5e6f65a31f328d6675c5628e12b14
leandrosimonato <leandrosimonato@gmail.com>
2021-09-05 16:18:00 -0300

    Criando o componente de Hello World.

--- 2.6. Criando o componente de Hello World/2.6/src/App.tsx
+++ 2.6. Criando o componente de Hello World/2.6/src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HelloWorld />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          olá, mundo!!!!!
         </p>
         <a
           className="App-link"
