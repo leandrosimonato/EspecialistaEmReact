866397a2e2a97feb2b03098a4b91cda2793cda0a
leandrosimonato <leandrosimonato@gmail.com>
2021-09-05 18:32:09 -0300

    A segurança do JSX.

---
+++ 2.11. A segurança do JSX/2.11/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Oi com segurança!!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
