52cbef3945837c251b88d3059ed5f47eecc4d43b
leandrosimonato <leandrosimonato@gmail.com>
2021-09-05 19:22:08 -0300

    Atualizando um componente em tempo real.

---
+++ 2.13. Atualizando um componente em tempo real/2.13/src/App.tsx
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './Clock';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Clock/>
+        <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Atualizando um componente em tempo real
+          </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
