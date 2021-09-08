895d8139e8ee5008d488476f0b99060e29353715
leandrosimonato <leandrosimonato@gmail.com>
2021-09-07 23:41:21 -0300

    Introdução à estilização de componentes (atributo style).

---
+++ 2.17. Introdução à estilização de componentes (atributo style)/2.17/src/App.tsx
@@ -0,0 +1,40 @@
+import logo from './heart.svg';
+import './App.css';
+import Post from './Components/Post';
+import { useState } from 'react';
+import { title } from 'process';
+import Card from './Components/Card';
+
+function App() {
+
+  const [post] = useState({
+    title: 'Título maneiro',
+    content: 'E um sub título'
+  })
+
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Card title={"Card"}>
+          <Post post={post} totalComments={12}/>
+        </Card>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Introdução à estilização de componentes (atributo style)
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
