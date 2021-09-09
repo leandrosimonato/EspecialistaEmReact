d4ca37afe601353ecfb81750d17615400bbbe700
leandrosimonato <leandrosimonato@gmail.com>
2021-09-09 09:45:06 -0300

    Introdução aos ciclos de vida com componentDidMount.

---
+++ 2.24. Introdução aos ciclos de vida com componentDidMount/2.24/src/App.tsx
@@ -0,0 +1,41 @@
+import logo from './heart.svg';
+import './App.css';
+import Post from './Components/Post';
+import { useState } from 'react';
+import { title } from 'process';
+import Card from './Components/Card';
+
+function App() {
+
+  const [post] = useState({
+    title: 'Título maneiro',
+    content: 'E um sub título'
+  })
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Card align="left" title={"Card"}>
+          <Post post={post} totalComments={12} />
+        </Card>
+
+        <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Introdução aos ciclos de vida com componentDidMount
+          </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
